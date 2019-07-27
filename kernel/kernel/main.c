354cbd3ed7ea44d0475e54bfddfe150189e9862b
Pedro Tammela <pctammela@gmail.com>
2019-07-27 15:08:01 -0300

    Adjust style for .c files

--- kernel/kernel/main.c
+++ kernel/kernel/main.c
@@ -1,6 +1,6 @@
 #include <lunaros/stdint.h>
 
-void main (uint64_t magic, uint64_t addr) {
+void main(uint64_t magic, uint64_t addr) {
    unsigned char *vga = (unsigned char *)0xb8000;
    ((void)magic);
    ((void)addr);
@@ -8,5 +8,6 @@ void main (uint64_t magic, uint64_t addr) {
    vga[0] = '.';
    vga[1] = 4;
 
-   while (1);
+   while (1)
+      ;
 }
