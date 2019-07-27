0dd59cbd9e8001f3c1964def4e0148385d068bba
Pedro Tammela <pctammela@gmail.com>
2019-07-26 21:06:12 -0300

    preparing for cpuid and memory topology

---
+++ kernel/include/lunaros/cpuid.h
@@ -0,0 +1,6 @@
+#ifndef _CPUID_H
+#define _CPUID_H
+
+#include <cpuid.h>
+
+#endif /* _CPUID_H */
--- kernel/kernel/main.c
+++ kernel/kernel/main.c
@@ -1,5 +1,10 @@
-void main (void) {
+#include <lunaros/stdint.h>
+
+void main (uint64_t magic, uint64_t addr) {
    unsigned char *vga = (unsigned char *)0xb8000;
+   ((void)magic);
+   ((void)addr);
+
    vga[0] = '.';
    vga[1] = 4;
 
