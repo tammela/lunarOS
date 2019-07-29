f85fa7f2b98d1587d3d694be50a0debd0f9d5e96
Pedro Tammela <pctammela@gmail.com>
2019-07-29 16:43:05 -0300

    Check if we are booted by a multiboot-compliant boot loader

--- kernel/kernel/main.c
+++ kernel/kernel/main.c
@@ -1,11 +1,15 @@
 #include <std/stdint.h>
+#include <multiboot/multiboot2.h>
 
 #include <lunaros/printf.h>
 #include <lunaros/video/tty.h>
 
-void main(uint64_t magic, uint64_t addr) {
-   ((void)magic);
+void main(uint32_t magic, uint32_t addr) {
    ((void)addr);
    init_tty();
+   if (magic != MULTIBOOT2_BOOTLOADER_MAGIC) {
+      printf("Invalid magic number");
+      return;
+   }
    puts("LunarOS Kernel\n");
 }
