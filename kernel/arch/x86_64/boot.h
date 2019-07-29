42c19dd032ec3435b1f0ee3bc488ee777aa8550c
Pedro Tammela <pctammela@gmail.com>
2019-07-29 16:24:15 -0300

    Replace magic numbers with multiboot2 definitions

--- kernel/arch/x86_64/boot.h
+++ kernel/arch/x86_64/boot.h
@@ -3,14 +3,7 @@
 
 /* Known addresses */
 #define KERNEL_PHYSICAL_START (0x0000000000400000)
-#define KERNEL_VIRTUAL_START  (0xFFFFFFFF80400000)
-
-/* We use multiboot 2 on x86_64 */
-#define MULTIBOOT2_HEADER_MAGIC 0xe85250d6
-
-#define MULTIBOOT_ARCHITECTURE_I386  0
-
-#define MULTIBOOT_HEADER_TAG_END 0
+#define KERNEL_VIRTUAL_START (0xFFFFFFFF80400000)
 
 /* GDT Utilities */
 #define GDT_FLAG_FOUR_KILOBYTE_GRANULARITY (1 << 3)
@@ -26,24 +19,18 @@
 #define GDT_ACCESS_DIRECTION_DOWN (1 << 2)
 #define GDT_ACCESS_READABLE_WRITABLE (1 << 1)
 
-#define DECLARE_GDT_ENTRY(base, limit, flags, access)                          \
-  (                                                                            \
-    (((((base)) >> 24) & 0xFF) << 56) |                                        \
-    ((((flags)) & 0xF) << 52) |                                                \
-    (((((limit)) >> 16) & 0xF) << 48) |                                        \
-    (((((access) | (1 << 4))) & 0xFF) << 40) |                                 \
-    ((((base)) & 0xFFF) << 16) |                                               \
-    (((limit)) & 0xFFFF)                                                       \
-  )
+#define DECLARE_GDT_ENTRY(base, limit, flags, access)                       \
+   ((((((base)) >> 24) & 0xFF) << 56) | ((((flags)) & 0xF) << 52) |         \
+    (((((limit)) >> 16) & 0xF) << 48) |                                     \
+    (((((access) | (1 << 4))) & 0xFF) << 40) | ((((base)) & 0xFFF) << 16) | \
+    (((limit)) & 0xFFFF))
 
 #define GDT_FIRST_ENTRY 0
 
-#define GDT_KERNEL_ENTRY                                                       \
-  DECLARE_GDT_ENTRY(0, 0,                                                      \
-                    GDT_FLAG_64BIT_MODE,                                       \
-                    GDT_ACCESS_PRESENT |                                       \
-                    GDT_ACCESS_PRIVILEGE_RING0 |                               \
-                    GDT_ACCESS_EXECUTABLE)                                     \
+#define GDT_KERNEL_ENTRY                                               \
+   DECLARE_GDT_ENTRY(0, 0, GDT_FLAG_64BIT_MODE,                        \
+                     GDT_ACCESS_PRESENT | GDT_ACCESS_PRIVILEGE_RING0 | \
+                         GDT_ACCESS_EXECUTABLE)
 
 #define GDT_TABLE_ALIGNMENT 4
 #define GDT_TABLE_SIZE 0x800
