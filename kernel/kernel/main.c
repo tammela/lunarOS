fff946e0b12d93ef319b42b39b68bc337b6bfa2c
mihalicyn <alexander@mihalicyn.com>
2019-08-08 14:29:08 +0300

    Issue #4, #8: Added multiboot info printing.
    It shows all information from multiboot2 bootloader except framebuffer info (not needed yet).

--- kernel/kernel/main.c
+++ kernel/kernel/main.c
@@ -7,9 +7,9 @@
 #include <lunaros/page.h>
 #include <lunaros/printf.h>
 #include <lunaros/tty.h>
+#include <lunaros/multiboot.h>
 
 void main(uint32_t magic, uint32_t addr) {
-   ((void)addr);
    cls();
    if (magic != MULTIBOOT2_BOOTLOADER_MAGIC) {
       puts("Invalid magic number");
@@ -17,4 +17,5 @@ void main(uint32_t magic, uint32_t addr) {
    }
    puts("LunarOS Kernel\n");
    cpu_init();
+   multiboot_print_info(addr);
 }
