2b1de305f53dd8474659df7f3872674ff9ab94e7
Pedro Tammela <pctammela@gmail.com>
2019-08-10 21:18:38 -0300

    Added initial implementation for panic().

--- kernel/include/lunaros/compiler.h
+++ kernel/include/lunaros/compiler.h
@@ -7,9 +7,10 @@
 
 /* Useful GCC attributes */
 #define __aligned       __attribute__((aligned))
-#define __packed        __attribute__((packed))
 #define __interrupt     __attribute__((interrupt))
 #define __inline        __attribute__((always_inline)) inline
+#define __noreturn      __attribute__((noreturn))
+#define __packed        __attribute__((packed))
 #define __printf(a, b)  __attribute__((format(printf, 1, 2)))
 
 #endif /* _COMPILER_H */
