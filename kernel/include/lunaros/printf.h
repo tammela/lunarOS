e9a81f6e6a90c5a55ce43e2f1a990087eb0b3d80
Pedro Tammela <pctammela@gmail.com>
2019-08-10 21:09:19 -0300

    Added implementation for vprintf().
    Also made printf() use vprintf().

--- kernel/include/lunaros/printf.h
+++ kernel/include/lunaros/printf.h
@@ -2,11 +2,14 @@
 #define _PRINTF_H
 
 #include <std/stdint.h>
+#include <std/stdarg.h>
+
 #include <lunaros/compiler.h>
 
 int uitoa(uintmax_t value, char *buf, int base);
 
 void puts(const char *s);
+void vprintf(const char *fmt, va_list args);
 __printf(1, 2) void printf(const char *fmt, ...);
 
 #endif /* _PRINTF_H */
