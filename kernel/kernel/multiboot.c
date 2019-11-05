579e898aac749c130d166aa7e534b2d6a2414a38
Pedro Tammela <pctammela@gmail.com>
2019-11-05 08:08:35 -0300

    Issue #29: improved multiboot parse code

--- kernel/kernel/multiboot.c
+++ kernel/kernel/multiboot.c
@@ -3,53 +3,43 @@
 #include <std/stdint.h>
 
 #include <lunaros/multiboot.h>
+#include <lunaros/page.h>
 #include <lunaros/printf.h>
 
+#define next_tag(t) \
+   ((struct multiboot_tag *)((multiboot_uint8_t *)t + ((t->size + 7) & ~7)))
+
+#define next_mmap_tag(m, t) \
+   ((uint8_t *)m + ((struct multiboot_tag_mmap *)t)->entry_size)
+
 void multiboot_mmap_iterate(unsigned long addr) {
-   struct multiboot_tag *tag;
+   struct multiboot_tag *tag = (struct multiboot_tag *)(addr + 8);
 
    if (addr & 7) {
       printf("WARN Unaligned mbi\n");
       return;
    }
 
-   for (tag = (struct multiboot_tag *)(addr + 8);
-        tag->type != MULTIBOOT_TAG_TYPE_END;
-        tag = (struct multiboot_tag *)((multiboot_uint8_t *)tag +
-                                       ((tag->size + 7) & ~7))) {
-      if (tag->type == MULTIBOOT_TAG_TYPE_MMAP) {
-         multiboot_memory_map_t *mmap;
-         for (mmap = ((struct multiboot_tag_mmap *)tag)->entries;
-              (multiboot_uint8_t *)mmap < (multiboot_uint8_t *)tag + tag->size;
-              mmap = (multiboot_memory_map_t
-                          *)((unsigned long)mmap +
-                             ((struct multiboot_tag_mmap *)tag)->entry_size)) {
-            printf(" base_addr = 0x%x%x,"
-                   " length = 0x%x%x, type = 0x%x\n",
-                   (unsigned)(mmap->addr >> 32),
-                   (unsigned)(mmap->addr & 0xffffffff),
-                   (unsigned)(mmap->len >> 32),
-                   (unsigned)(mmap->len & 0xffffffff), (unsigned)mmap->type);
-         }
+   for (; tag->type != MULTIBOOT_TAG_TYPE_END; tag = next_tag(tag)) {
+      if (tag->type != MULTIBOOT_TAG_TYPE_MMAP)
+         continue;
+      multiboot_memory_map_t *mmap =
+          ((struct multiboot_tag_mmap *)tag)->entries;
+      while ((uint8_t *)mmap < ((uint8_t *)tag + tag->size)) {
+         mmap = (multiboot_memory_map_t *)next_mmap_tag(mmap, tag);
       }
    }
 }
 
 void multiboot_print_info(unsigned long addr) {
-   struct multiboot_tag *tag;
-   unsigned size;
+   struct multiboot_tag *tag = (struct multiboot_tag *)(addr + 8);
 
    if (addr & 7) {
       printf("WARN Unaligned mbi\n");
       return;
    }
 
-   size = *(unsigned *)addr;
-   printf("Announced mbi size 0x%x\n", size);
-   for (tag = (struct multiboot_tag *)(addr + 8);
-        tag->type != MULTIBOOT_TAG_TYPE_END;
-        tag = (struct multiboot_tag *)((multiboot_uint8_t *)tag +
-                                       ((tag->size + 7) & ~7))) {
+   for (; tag->type != MULTIBOOT_TAG_TYPE_END; tag = next_tag(tag)) {
       switch (tag->type) {
       case MULTIBOOT_TAG_TYPE_CMDLINE:
          printf("Command line = %s\n",
@@ -60,7 +50,7 @@ void multiboot_print_info(unsigned long addr) {
                 ((struct multiboot_tag_string *)tag)->string);
          break;
       case MULTIBOOT_TAG_TYPE_MODULE:
-         printf("Module at 0x%x-0x%x. Command line %s\n",
+         printf("Module at %x-%x. Command line %s\n",
                 ((struct multiboot_tag_module *)tag)->mod_start,
                 ((struct multiboot_tag_module *)tag)->mod_end,
                 ((struct multiboot_tag_module *)tag)->cmdline);
@@ -71,22 +61,15 @@ void multiboot_print_info(unsigned long addr) {
                 ((struct multiboot_tag_basic_meminfo *)tag)->mem_upper);
          break;
       case MULTIBOOT_TAG_TYPE_MMAP: {
-         multiboot_memory_map_t *mmap;
-
-         printf("mmap\n");
-
-         for (mmap = ((struct multiboot_tag_mmap *)tag)->entries;
-              (multiboot_uint8_t *)mmap < (multiboot_uint8_t *)tag + tag->size;
-              mmap = (multiboot_memory_map_t
-                          *)((unsigned long)mmap +
-                             ((struct multiboot_tag_mmap *)tag)->entry_size))
-            printf(" base_addr = 0x%x%x,"
-                   " length = 0x%x%x, type = 0x%x\n",
-                   (unsigned)(mmap->addr >> 32),
-                   (unsigned)(mmap->addr & 0xffffffff),
-                   (unsigned)(mmap->len >> 32),
-                   (unsigned)(mmap->len & 0xffffffff), (unsigned)mmap->type);
-      } break;
+         multiboot_memory_map_t *mmap =
+             ((struct multiboot_tag_mmap *)tag)->entries;
+         while ((uint8_t *)mmap < ((uint8_t *)tag + tag->size)) {
+            printf(" base_addr = %llx, length = %llx, type = %x\n", mmap->addr,
+                   mmap->len, mmap->type);
+            mmap = (multiboot_memory_map_t *)next_mmap_tag(mmap, tag);
+         }
+         break;
+      }
       }
    }
 }
