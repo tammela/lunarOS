6c9452b8c3ede99e6ca8af46df9c016774dda00d
Pedro Tammela <pctammela@gmail.com>
2019-12-31 12:07:41 -0300

    mm: add function to invalidate single page on TLB
    Adds vmm_invlpg() to invalidate single page on TLB. This is very
    specific to the x86 architecture and should be improved in the future.
    
    Signed-off-by: Pedro Tammela <pctammela@gmail.com>

--- kernel/include/lunaros/vmm.h
+++ kernel/include/lunaros/vmm.h
@@ -13,6 +13,19 @@ struct vmm_map_t {
    uint16_t pte;        /* pte index allocated */
 };
 
+/*
+** Invalidate page on TLB cache
+*/
+void vmm_invlpg(uint64_t addr);
+
+/*
+** Fetch the first free map in the page table structures
+*/
 uint32_t vmm_fetch_map(vmm_map_t *map);
+
+/*
+** Maps the physical address `page` to `map`. Returns the virtual address.
+*/
 void *vmm_assign_map(vmm_map_t *map, void *page, ppt_t type);
+
 void vmm_init(pde_t *reserved);
