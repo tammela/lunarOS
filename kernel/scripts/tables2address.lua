11fc14f45afe3085494d78eec555fb100f903c5c
Pedro Tammela <pctammela@gmail.com>
2020-02-03 19:30:52 +0000

    scripts: add 4-KiB address / page table converters
    good scripts for debugging
    
    Signed-off-by: Pedro Tammela <pctammela@gmail.com>

---
+++ kernel/scripts/tables2address.lua
@@ -0,0 +1,28 @@
+#!/usr/bin/lua5.3
+
+function error()
+   print("Input is malformed")
+   os.exit(-1)
+end
+
+function help()
+   print("No arguments")
+end
+
+if not arg[1] or type(arg[1]) ~= "string" then
+   help()
+   os.exit(-1)
+end
+
+local address = 0
+local t = load(arg[1], "argument chunk", "t")()
+address = address | (t.pml4 << 39)
+address = address | (t.pdpte << 30)
+address = address | (t.pde << 21)
+address = address | (t.pte << 12)
+
+if t.pml4 > 255 then -- higher half
+   address = address | (0xFFFF << 48)
+end
+
+print(string.format("%x", address))
