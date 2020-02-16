9e21cdc0b19c9cc874255fbbe29d3245db889997
Pedro Tammela <pctammela@gmail.com>
2020-02-16 17:23:16 +0000

    import: import NetBSD's container_of()
    Signed-off-by: Pedro Tammela <pctammela@gmail.com>

--- kernel/include/lunaros/kernel.h
+++ kernel/include/lunaros/kernel.h
@@ -1,8 +1,27 @@
 #pragma once
 
+#include <std/stddef.h>
+
 #include <lunaros/compiler.h>
 
 /* See docs/virtual-memory.txt */
 #define _VM_KERNEL_START        (0xFFFF800000000000)
 
 void __noreturn panic(const char *fmt, ...);
+
+
+/* container_of() from NetBSD */
+
+#define __validate_container_of(PTR, TYPE, FIELD)                 \
+   (0 * sizeof((PTR) - &((TYPE *)(((char *)(PTR)) -               \
+      offsetof(TYPE, FIELD)))->FIELD))
+#define __validate_const_container_of(PTR, TYPE, FIELD)           \
+   (0 * sizeof((PTR) - &((const TYPE *)(((const char *)(PTR)) -   \
+      offsetof(TYPE, FIELD)))->FIELD))
+
+#define container_of(PTR, TYPE, FIELD)                            \
+   ((TYPE *)(((char *)(PTR)) - offsetof(TYPE, FIELD))             \
+       + __validate_container_of(PTR, TYPE, FIELD))
+#define const_container_of(PTR, TYPE, FIELD)                      \
+   ((const TYPE *)(((const char *)(PTR)) - offsetof(TYPE, FIELD)) \
+       + __validate_const_container_of(PTR, TYPE, FIELD))
