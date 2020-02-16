8183273511532c4d492aefd50cb27d9b78f7dd2d
Pedro Tammela <pctammela@gmail.com>
2020-02-16 17:24:44 +0000

    mm: add implementation for page_alloc() and page_free()
    Signed-off-by: Pedro Tammela <pctammela@gmail.com>

--- kernel/kernel/mm.c
+++ kernel/kernel/mm.c
@@ -1,6 +1,10 @@
+#include <std/string.h>
+
 #include <lunaros/mm.h>
 #include <lunaros/vmm.h>
 #include <lunaros/page.h>
+#include <lunaros/pagetable.h>
+#include <lunaros/kernel.h>
 
 /*
 ** Allocate memory in a 2-page reserved area.
@@ -18,7 +22,37 @@ void *mm_reserved_alloc(size_t sz) {
    return ret;
 }
 
+/*
+** Direct map the entire physical memory supported in 1GiB pages.
+** On 64-bit we hardcode the limit to 512 GiB so far, but it's
+** guaranteed to be in a reserved an area of about 64TiB.
+** Just like Linux...
+*/
+static __inline void direct_map_everything(void) {
+   pml4e_t pml4e = {0};
+   huge_pdpte_t entry = {0};
+   huge_pdpte_t *pages = mm_reserved_alloc(512 * sizeof(huge_pdpte_t));
+   if (pages == NULL)
+      panic("Not enough memory to bootstrap memory");
+   for (int i = 0; i < 1; i++) {
+      for (int j = 0; j < 512; j++) {
+         entry.present = true;
+         entry.rw = true;
+         entry.ps = true; /* 1 GiB page */
+         entry.base = j + (512 * i);
+         pages[j + (512 * i)] = entry;
+      }
+      uint64_t p = (uint64_t)vmm_virt2phys(pages);
+      memcpy(&pml4e, &p, sizeof(p));
+      pml4e.present = true;
+      pml4e.rw = true;
+      vmm_direct_map(pml4e);
+   }
+   vmm_flush_tlb();
+}
+
 void mm_init(physmem_layout_t **layouts, size_t physoff, pte_t *reserved) {
+   direct_map_everything();
    /* Virtual memory manager */
    vmm_init(reserved);
    /* Physical page allocator */
