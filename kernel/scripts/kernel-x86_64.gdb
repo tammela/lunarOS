09d854be1204cf3e45bf5fc7f382f1b872b9563a
Pedro Tammela <pctammela@gmail.com>
2020-03-01 19:43:16 +0000

    improvement: update QoL scripts
    Signed-off-by: Pedro Tammela <pctammela@gmail.com>

--- kernel/scripts/kernel-x86_64.gdb
+++ kernel/scripts/kernel-x86_64.gdb
@@ -1,2 +1,2 @@
 add-symbol-file kernel/lunar.kernel
-target remote | qemu-system-x86_64 -S -gdb stdio -m 128M -cdrom lunarOS.iso -d cpu_reset
+target remote | qemu-system-x86_64 -S -gdb stdio -monitor tcp::1337,server,nowait -m 128M -cdrom lunarOS.iso -d cpu_reset
--- kernel/scripts/tables2address.lua
+++ kernel/scripts/tables2address.lua
@@ -17,9 +17,9 @@ end
 local address = 0
 local t = load(arg[1], "argument chunk", "t")()
 address = address | (t.pml4 << 39)
-address = address | (t.pdpte << 30)
-address = address | (t.pde << 21)
-address = address | (t.pte << 12)
+address = address | (t.pdpt << 30)
+address = address | (t.pd << 21)
+address = address | (t.pt << 12)
 
 if t.pml4 > 255 then -- higher half
    address = address | (0xFFFF << 48)
